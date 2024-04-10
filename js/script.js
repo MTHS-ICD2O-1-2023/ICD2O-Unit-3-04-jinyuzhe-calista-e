dc7b8d2b50109b07a9e1d4e2f3dbb94c795f7111
Calista.E <159185314+jinyuzhe-calista-e@users.noreply.github.com>
2024-04-10 18:12:00 +0000

    rg

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Calista.E
+// Created on: Apr 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function convert Fahrenheit to Celsius.
+ */
+function calculateFahrenheit() {
+  // input
+  const fahrenheit = parseFloat(document.getElementById('fahrenheit').value)
+
+  // process
+  const celsius = ( fahrenheit - 32 ) * 5 / 9
+
+  // output
+  document.getElementById('celsius').innerHTML = 'Celsius is: ' + celsius.toFixed(2) + ' °C'
+}
